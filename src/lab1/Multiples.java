2e7be2ffd09519d351a9be7cbc592f9b5903a869
Shawn <shawn.jin@mail.utoronto.ca>
2025-09-08 17:45:09 -0400

    Task 4

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    static int multiples(int n, int a, int b) {
+        int curr = 0;
+        for (int i = 1; i <= n; i++) {
+            boolean divisibleBya = i % a == 0;
+            boolean divisibleByb = i % b == 0;
+            if  (divisibleBya && divisibleByb) {
+                curr++;
+            }
+        }
+        return curr;
+    }
+}
